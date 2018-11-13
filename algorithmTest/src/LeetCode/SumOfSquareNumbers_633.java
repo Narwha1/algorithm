0768d7e4a65dd5b4796a5102ebbbd3fa93f5e64e
张靖宜 <jingyi.zhang01@hand-china.com>
2018-11-13 16:47:35 +0800

    commit

---
+++ algorithmTest/src/LeetCode/SumOfSquareNumbers_633.java
@@ -0,0 +1,24 @@
+package LeetCode;
+
+public class SumOfSquareNumbers_633 {
+    public static boolean judgeSquareSum(int c) {
+        int i = 0, j = (int) Math.sqrt(c);
+        while (i <= j) {
+            int powSum = i * i + j * j;
+            if (powSum == c) {
+                return true;
+            } else if (powSum > c) {
+                j--;
+            } else {
+                i++;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int c = 3;
+        boolean a = judgeSquareSum(c);
+        System.out.println(a);
+    }
+}
